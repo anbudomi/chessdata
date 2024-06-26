4bbd89043158dee74f1e2d6283f59d022228f741
anbudomi <domenictheis@gmail.com>
2024-06-26 21:14:21 +0200

    First changes to code to play around with chess.com api

--- code/collect_data.py
+++ code/collect_data.py
@@ -1,1 +1,31 @@
 
+import sqlite3
+import requests
+from collections import deque
+
+# APIs
+
+#chess.com api
+chess_com_api = 'https://api.chess.com/pub/player/'
+#api is a string that will have to get modified for each players profile
+
+
+#lichess api
+#idk yet
+
+
+
+#The following code is going to be a test for the JSON
+
+def fetch_games():
+    
+    url = chess_com_api + 'gmhikaru/games'
+    
+    print(url)
+    
+    response = response.json()
+    
+    print(response)
+    
+
+fetch_games()
