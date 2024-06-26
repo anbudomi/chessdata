4eb274ed434fdf01f00001b327188d6d61013247
anbudomi <domenictheis@gmail.com>
2024-06-26 22:00:38 +0200

    further trial and error with chess.com api

--- code/collect_data.py
+++ code/collect_data.py
@@ -1,5 +1,6 @@
 
 import sqlite3
+import json
 import requests
 from collections import deque
 
@@ -7,25 +8,26 @@ from collections import deque
 
 #chess.com api
 chess_com_api = 'https://api.chess.com/pub/player/'
+GamesString = 'games/'
+CurrentUsername = ''
 #api is a string that will have to get modified for each players profile
+#to access games the api string will need to have the following format:
+#'https://api.chess.com/pub/player/games/yyyy/mm'
+#Note: its easiest to start with the current date and then go back until you can't find games (for a year)
 
 
-#lichess api
-#idk yet
-
-
-
-#The following code is going to be a test for the JSON
+#The following code is going to be a test for the JSON data
 
 def fetch_games():
     
-    url = chess_com_api + 'gmhikaru/games'
+    url = chess_com_api + 'hikaru/games/2023/10'
     
     print(url)
     
-    response = response.json()
+    response = requests.get(url)
+    GameData = response.json()
     
-    print(response)
+    print(GameData)
     
 
 fetch_games()
