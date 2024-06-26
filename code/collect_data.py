7fa1f0e0092831eb1f92809b8d546238ccb79d55
anbudomi <domenictheis@gmail.com>
2024-06-26 23:31:35 +0200

    Getting access to chesscom api and gather first usernames Also: Starting to work on algorithm to run through usernames

--- code/collect_data.py
+++ code/collect_data.py
@@ -1,33 +1,83 @@
-
-import sqlite3
 import json
 import requests
 from collections import deque
+from datetime import datetime
 
 # APIs
 
 #chess.com api
 chess_com_api = 'https://api.chess.com/pub/player/'
 GamesString = 'games/'
-CurrentUsername = ''
+starting_username = 'hikaru'
 #api is a string that will have to get modified for each players profile
 #to access games the api string will need to have the following format:
 #'https://api.chess.com/pub/player/games/yyyy/mm'
-#Note: its easiest to start with the current date and then go back until you can't find games (for a year)
 
 
-#The following code is going to be a test for the JSON data
+#The following code is just for figuring out the chess.com API and playing around
 
-def fetch_games():
+def test_fetch_games():
     
-    url = chess_com_api + 'hikaru/games/2023/10'
-    
+    #url = chess_com_api + 'hikaru/games/2023/10'
+    url = chess_com_api + 'hikaru/'
     print(url)
     
-    response = requests.get(url)
-    GameData = response.json()
-    
-    print(GameData)
-    
+    response = requests.get(url, headers = {'User-Agent': 'username: ChessMaid, email: domkeychess@gmail.com'})
+    print(response)
 
-fetch_games()
+    GameData = response.json()
+    #print(GameData['games'])
+    print(GameData['joined'])
+
+    #GameData = GameData['games']
+
+    #for i in range(0, len(GameData)):
+        #user = GameData[i]['white']['username']
+        #print(user)
+
+
+def fetch_games():
+    start_year = '2024'
+    start_month = '06'
+    end_year = ''
+    end_month = ''
+
+    current_username = ''
+    
+    visited = set()
+    queue = deque([starting_username])
+    usernames = set()
+
+    while queue:
+        current_username = queue.popleft()
+
+        url_games = chess_com_api + current_username + 'games/'
+        url_profile = chess_com_api + current_username
+
+        if current_username in visited:
+            continue
+
+        visited.add(current_username)
+        usernames.add(current_username)
+
+    	date_timestamp = get_date_timestamp(url_profile)
+
+        end_year = get_joined_year(date_timestamp)
+        end_month = get_joined_month(date_timestamp)
+
+def get_joined_month(date_code):
+    #return str month MM
+    return
+
+def get_joined_year(date_code):
+    #return str year YY
+    return
+
+def get_date_timestamp(url):
+        response = requests.get(url, headers = {'User-Agent': 'username: ChessMaid, email: domkeychess@gmail.com'})
+        temp_timestamp = response.json()
+        temp_timestamp = int(date_timestamp['joined'])
+        return temp_timestamp
+
+test_fetch_games()
+#fetch_games()
